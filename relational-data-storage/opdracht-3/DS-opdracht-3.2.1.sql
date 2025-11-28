77b91425824bc38610dddc954962dd6e56e36612
Nino <ninobleus@gmail.com>
2025-11-28 15:39:07 +0100

    opdracht 1 +2 + 3

---
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.1.sql
@@ -0,0 +1,6 @@
+SELECT 	name,
+		straat, 
+        huisnr, 
+        postcode 
+FROM mhl_suppliers
+where city_ID = 104;
---
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.6.sql
@@ -0,0 +1,3 @@
+SELECT name, straat, huisnr, postcode
+FROM mhl_suppliers
+WHERE huisnr BETWEEN 10 AND 20 or huisnr > 100; 
